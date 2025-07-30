4dbe872cf10faa6f956d1337a1562e6626b5e60e
wanggengen <gengen.wang@outlook.com>
2025-07-30 13:49:21 +0800

    add log level

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,21 @@ fn main() {
     let os = std::env::consts::OS;
 
     // Windows平台使用log库打印
-    #[cfg(all(windows, feature = "windows-log"))]
+    #[cfg(all(target_os = "windows", feature = "windows-log"))]
     {
-        env_logger::init();
-        log::info!("current os is {}", os);
+        env_logger::Builder::from_default_env()
+            .filter_level(log::LevelFilter::Info)
+            .init();
+        log::info!("log: current os is {}", os);
     }
 
     // Linux平台使用tracing库打印
     #[cfg(all(target_os = "linux", feature = "linux-tracing"))]
     {
-        tracing_subscriber::fmt::init();
-        tracing::info!("current os is {}", os);
+        tracing_subscriber::fmt()
+            .with_max_level(tracing::Level::INFO)
+            .init();
+        tracing::info!("traceing: current os is {}", os);
     }
 
     // 通用输出
