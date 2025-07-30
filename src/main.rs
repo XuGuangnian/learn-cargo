f9288deae78e8469260472a8ad50127bb126c1d1
wanggengen <gengen.wang@outlook.com>
2025-07-30 13:42:39 +0800

    cargo features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let os = std::env::consts::OS;
+
+    // Windows平台使用log库打印
+    #[cfg(all(windows, feature = "windows-log"))]
+    {
+        env_logger::init();
+        log::info!("current os is {}", os);
+    }
+
+    // Linux平台使用tracing库打印
+    #[cfg(all(target_os = "linux", feature = "linux-tracing"))]
+    {
+        tracing_subscriber::fmt::init();
+        tracing::info!("current os is {}", os);
+    }
+
+    // 通用输出
+    println!("os: {}", os);
 }
